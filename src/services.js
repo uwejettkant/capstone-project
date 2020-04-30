b70d12f2b503e426be03a93e8d22947de6bc04d8
Uwe Jettkant <uj@Uwes-MBP.fritz.box>
2020-04-30 20:31:49 +0200

    initial commit
    Signed-off-by: Uwe Jettkant <uj@Uwes-MBP.fritz.box>

---
+++ src/services.js
@@ -0,0 +1,14 @@
+const userTasks = [
+  {
+    id: 2, 
+    headline: 'Finde dein Produkt',
+    todo: 'Um dir den Einstieg zu erleichtern, klicke bitte auf den folgenden Link. Du wirst auf die größte asiatische Handelsplattform "Alibaba.com" weitergeleitet.',
+  },
+  {
+    id: 3, 
+    headline: 'Finde dein Produkt',
+    todo: ''
+  },
+]
+
+export default userTasks 
